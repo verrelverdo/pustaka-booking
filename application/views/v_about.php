777370e61ac8d7c82f10c6c3b4ef3f66122b086b
verrelverdo <verrelvrdo28@gmail.com>
2022-10-08 20:31:57 +0700

    Tugas 4

---
+++ application/views/v_about.php
@@ -0,0 +1,21 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Verrel</li>
+        <li>Nama Belakang : Verdo</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jl. Raya Cikarang - Cibarusah</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bekasi</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Sepakbola</li>
+    </ul>
+</section>
